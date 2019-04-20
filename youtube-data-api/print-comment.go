89a620ccef60caed2a98feb7a94f15442105924a
Doarakko <2wua4nlyi@gmail.com>
2019-04-21 04:26:03 +0900

    get config var from .env

--- youtube-data-api/print-comment.go
+++ youtube-data-api/print-comment.go
@@ -4,29 +4,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"google.golang.org/api/googleapi/transport"
 	"google.golang.org/api/youtube/v3"
 )
 
-var (
-	client = &http.Client{
-		Transport: &transport.APIKey{Key: developerKey},
+func newYoutubeService() *youtube.Service {
+	client := &http.Client{
+		Transport: &transport.APIKey{Key: os.Getenv("YOUTUBE_API_KEY")},
 	}
-	service, err = youtube.New(client)
-	order        = "relevance"
-	searchTerms  = ""
-	maxResults   = int64(5)
-)
 
-const developerKey = "your key"
+	service, err := youtube.New(client)
+	if err != nil {
+		log.Fatalf("Unable to create YouTube service: %v", err)
+	}
+
+	return service
+}
 
 func printComments(VideoID string) {
+	service := newYoutubeService()
 	call := service.CommentThreads.List("id,snippet").
 		VideoId(VideoID).
-		SearchTerms(searchTerms).
-		Order(order).
-		MaxResults(maxResults)
+		Order("relevance").
+		SearchTerms("草").
+		MaxResults(10)
 	response, err := call.Do()
 	if err != nil {
 		log.Fatalf("%v", err)
@@ -37,11 +41,16 @@ func printComments(VideoID string) {
 		text := item.Snippet.TopLevelComment.Snippet.TextDisplay
 		likeCnt := item.Snippet.TopLevelComment.Snippet.LikeCount
 		replyCnt := item.Snippet.TotalReplyCount
-		fmt.Printf("\"%v\" by %v\nいいね数: %d 返信数: %d\n\n", text, authorName, likeCnt, replyCnt)
+		fmt.Printf("\"%v\" by %v\nいいね数: %v 返信数: %v\n\n", text, authorName, likeCnt, replyCnt)
 	}
 }
 
 func main() {
-	videoID := "Qs3sShlgKGk"
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	videoID := "_fJNAf6_4NU"
 	printComments(videoID)
 }

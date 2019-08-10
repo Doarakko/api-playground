691a733d44f0e2cd3d651fce04f7c5ac586c21f1
Doarakko <2wua4nlyi@gmail.com>
2019-08-10 17:53:03 +0900

    add tweet.py

---
+++ twitter-api/tweet.py
@@ -0,0 +1,18 @@
+from requests_oauthlib import OAuth1Session
+
+
+TWITTER_API_KEY = os.environ['TWITTER_API_KEY']
+TWITTER_API_SECRET = os.environ['TWITTER_API_SECRET']
+TWITTER_ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+TWITTER_ACCESS_TOKEN_SECRET = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+
+
+def post_twitter(message):
+    endpoint = 'https://api.twitter.com/1.1/'
+    url = endpoint + 'statuses/update.json'
+
+    params = {"status": message}
+    session = OAuth1Session(TWITTER_API_KEY, TWITTER_API_SECRET,
+                            TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+
+    request = session.post(url=url, params=params)
